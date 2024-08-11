226f2fbe447c507eef0f8303cf158c905c806af6
SuperNoobTT <151025879+SuperNoobTT@users.noreply.github.com>
2024-08-11 13:42:41 +0800

    Refactor grid to allow chars and image paths, commented out some of the print statements in preparation for egui frontend. Not working! (#7)

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-use crate::{grid::{Grid, States}, utils::input_helper};
+use crate::{grid::{Grid, States, Cell, Icon}, utils::input_helper};
 
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) enum Turns {
@@ -10,8 +10,8 @@ pub(crate) enum Turns {
 pub(crate) struct Board {
     pub(crate) grid: Grid,
     pub(crate) turn: Turns,
-    player_icon: char,
-    cpu_icon: char,
+    player_icon: Icon,
+    cpu_icon: Icon,
 }
 
 impl Default for Board {
@@ -19,8 +19,8 @@ impl Default for Board {
         Self { 
             grid: Grid::default(), 
             turn: Turns::Player,
-            player_icon: '❌',
-            cpu_icon: '⭕'
+            player_icon: Icon::Char('❌'),
+            cpu_icon: Icon::Char('⭕')
         }
     }
 }
@@ -31,49 +31,50 @@ impl Board {
         self.new_turn()
     }
 
+    #[deprecated(since = "Started move to EGUI")]
     pub(crate) fn custom_icon(&mut self) {
         let user_fn = |input: String| {
             ["player", "cpu"].contains(&input.to_lowercase().as_str())
             .then(|| input.to_lowercase())
             .ok_or("Invalid user !!!")
         };
-        loop {
-            println!("The current icons are: \nPlayer: {}, \nCPU: {}", self.player_icon, self.cpu_icon);
+        // loop {
+        //     // println!("The current icons are: \nPlayer: {}, \nCPU: {}", self.player_icon, self.cpu_icon);
 
-            let input = input_helper("Would you like to customise the game icons? (Enter Y/N)", |input: String| {
-                match input.to_lowercase().as_str() {
-                    "y"|"n" => Ok(input.to_lowercase()),
-                    _ => Err("Unrecognised input!")
-                }
-            });
+        //     let input = input_helper("Would you like to customise the game icons? (Enter Y/N)", |input: String| {
+        //         match input.to_lowercase().as_str() {
+        //             "y"|"n" => Ok(input.to_lowercase()),
+        //             _ => Err("Unrecognised input!")
+        //         }
+        //     });
 
-            if input == "n".to_string() {
-                break
-            } else {
-                assert!(input == "y".to_string()); //This should always be true bc of closure check
-                //Get the new unicode icon
-                let icon: char = input_helper("Please enter the icon you would like to use (a single unicode character)", |icon: char| {
-                    if icon.is_numeric() {
-                        Err("Digits are reserved for the game, please choose a non-digit unicode character!")
-                    } else {
-                        Ok(icon)
-                    }
-                });
-                //Get the user who will be represented by the icon
-                let user: String = input_helper("Please select whether the icon should be for the cpu or player", user_fn);
-                //Match the user and update icons appropriately
-                if user == "player".to_string() {
-                    self.player_icon = icon;
-                } else {
-                    assert!(user == "cpu".to_lowercase().to_string()); //Should never fail
-                    self.cpu_icon = icon;
-                };
-            }
-        }
+        //     if input == "n".to_string() {
+        //         break
+        //     } else {
+        //         assert!(input == "y".to_string()); //This should always be true bc of closure check
+        //         //Get the new unicode icon
+        //         let icon: char = input_helper("Please enter the icon you would like to use (a single unicode character)", |icon: char| {
+        //             if icon.is_numeric() {
+        //                 Err("Digits are reserved for the game, please choose a non-digit unicode character!")
+        //             } else {
+        //                 Ok(icon)
+        //             }
+        //         });
+        //         //Get the user who will be represented by the icon
+        //         let user: String = input_helper("Please select whether the icon should be for the cpu or player", user_fn);
+        //         //Match the user and update icons appropriately
+        //         if user == "player".to_string() {
+        //             self.player_icon = grid::Icon::Char(icon);
+        //         } else {
+        //             assert!(user == "cpu".to_lowercase().to_string()); //Should never fail
+        //             self.cpu_icon = icon;
+        //         };
+        //     }
+        // }
     }
 
     fn new_turn(&mut self) -> GameResult {
-        println!("Current grid: \n{:}", &self.grid); //Print the current grid
+        // println!("Current grid: \n{:}", &self.grid); **Deprecated!**
         //Match on the current turn and run the appropriate code
         match self.turn {
             Turns::Player => self.player_turn(),
@@ -87,13 +88,14 @@ impl Board {
             },
             States::Win(icon) => { //Someone wins!
                 //Match on the icons and return the winner to update the game
-                println!("{icon} wins! \n");
+
+                // println!("{icon} wins! \n"); **deprecated**
                 if icon == self.player_icon {
                     println!("You WIN :> \n");
                     return GameResult::PlayerWin;
                 } else {
                     assert_eq!(icon, self.cpu_icon); // This is always the cpu_icon 
-                    println!("The final board: \n{:} \n", self.grid);
+                    // println!("The final board: \n{:} \n", self.grid); **commented to remove error lol
                     println!("The CPU Wins :P \n");
                     return GameResult::CPUWin;
                 }
@@ -110,15 +112,14 @@ impl Board {
                 return Err("Invalid digit!");
             }
             let input = input - 1;
-            let (i, j) = (input/3, input%3);
-            if self.grid[i][j].is_numeric() {
-                return Ok((i, j));
+            if let Cell::Uninitialised = self.grid[input] {
+                return Ok(input);
             } 
             Err("Selected an already selected box!")
         };
         //Handle the user's input and grab the indexes to update the grid
-        let (i, j): (usize, usize) = input_helper("Please choose an unselected box and enter its digit", choice_fn);
-        self.grid[i][j] = self.player_icon;
+        let i = input_helper("Please choose an unselected box and enter its digit", choice_fn);
+        self.grid[i] = Cell::new_player(self.player_icon);
         //It's now the cpu's turn
         self.turn = Turns::Cpu;
     }
@@ -126,60 +127,55 @@ impl Board {
     fn cpu_turn(&mut self) {
         //Find the 'best' move (according to our algorithm) and update the grid accordingly
         let best_move = self.find_best_move();
-        let (i, j) = best_move;
-        self.grid[i][j] = self.cpu_icon;
+        self.grid[best_move] = Cell::new_cpu(self.cpu_icon);
         self.turn = Turns::Player;
         //Output the chosen box for clarity
-        println!("The cpu chose box number: {} \n", (best_move.0*3 + best_move.1) + 1);
+        println!("The cpu chose box number: {} \n", best_move+1);
     }
 
     ///Super suboptimal algorithm, but I'm very lazy :P
-    fn find_best_move(&self) -> (usize, usize) {
+    fn find_best_move(&self) -> usize {
         // Check for immediate win
-        if let Some(winning_move) = self.find_winning_move(self.cpu_icon) {
+        if let Some(winning_move) = self.find_winning_move(Cell::new_cpu(self.cpu_icon)) {
             return winning_move;
         }
 
         // Block opponent's winning move
-        if let Some(blocking_move) = self.find_winning_move(self.player_icon) {
+        if let Some(blocking_move) = self.find_winning_move(Cell::new_player(self.player_icon)) {
             return blocking_move;
         }
 
         // If center is free, take it
-        if self.grid[1][1].is_numeric() {
-            return (1, 1);
+        if let Cell::Uninitialised = self.grid[4] {
+            return 4;
         }
 
         // Prefer corners
         const CORNERS: [usize; 4] = [0, 2, 6, 8];
         for &corner in CORNERS.iter() {
-            let (i, j) = (corner / 3, corner % 3);
-            if self.grid[i][j].is_numeric() {
-                return (i, j);
+            if let Cell::Uninitialised = self.grid[corner] {
+                return corner;
             }
         }
 
         // Take any available space (only edges left so can skip even indexes)
-        let idx: usize = (1..8).step_by(2)
+        (1..8).step_by(2)
             .find(|&n| {
-            let (i, j) = (n / 3, n % 3);
-            self.grid[i][j].is_numeric()
+            let curr_cell: Cell = self.grid[n];
+            matches!(Cell::Uninitialised, curr_cell)
             })
-            .expect("There should always be at least one empty area");
-        (idx/3, idx%3)
+            .expect("There should always be at least one empty area")
     }
 
-    fn find_winning_move(&self, player: char) -> Option<(usize, usize)> {
-        for i in 0..3 {
-            for j in 0..3 {
-                if self.grid[i][j].is_numeric() {
-                    //Clone bc I'm too lazy to find an in-place check :>
-                    let mut future_grid: Grid = self.grid.clone();
-                    future_grid[i][j] = player;
-                    //If there's an immediate win for either player, return the coords!
-                    if let States::Win(_) = future_grid.get_state() {
-                        return Some((i, j));
-                    }
+    fn find_winning_move(&self, player: Cell) -> Option<usize> {
+        for i in 0..9 {
+            if let Cell::Uninitialised = self.grid[i] {
+                //Clone bc I'm too lazy to find an in-place check :>
+                let mut future_grid: Grid = self.grid.clone();
+                future_grid[i] = player;
+                //If there's an immediate win for either player, return the coords!
+                if let States::Win(_) = future_grid.get_state() {
+                    return Some(i);
                 }
             }
         }
--- src/grid.rs
+++ src/grid.rs
@@ -1,34 +1,61 @@
-use std::{fmt::Display, ops::{Index, IndexMut}};
-use unicode_width::UnicodeWidthChar;
+use std::ops::{Index, IndexMut};
+use eframe::egui::*;
 
-///Used to handle everything that the tic tac toe grid needs!
-#[derive(Debug, Clone, PartialEq)]
-pub(crate) struct Grid([[char; 3]; 3]);
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(crate) enum Icon {
+    Char(char),
+    Image(&'static str)
+}
 
-///Save some logic later on by implementing basic formatting
-impl Display for Grid {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (i, row) in self.0.iter().enumerate() {
-            for (j, &cell) in row.iter().enumerate() {
-                let width = cell.width().unwrap_or(1);
-                let padding = 3 - width;
-                write!(f, " {}{} ", cell, " ".repeat(padding))?;
-                if j < 2 {
-                    write!(f, "|")?;
-                }
-            }
-            if i < 2 {
-                writeln!(f, "\n-----+-----+-----")?;
-            }
-        }
-        Ok(())
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(crate) enum Cell {
+    Player(Icon),
+    Cpu(Icon), 
+    Uninitialised
+}
+
+impl Cell {
+    pub fn new_player(icon: Icon) -> Self {
+        Self::Player((icon))
+    }
+
+    pub fn new_cpu(icon: Icon) -> Self {
+        Self::Cpu(icon)
     }
 }
 
+///Used to handle everything that the tic tac toe grid needs!
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct Grid([Cell; 9]);
+
+///Save some logic later on by implementing basic formatting
+// #[deprecated(since = "commit 5", note = "new function using egui in progress")]
+// impl Display for Grid {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         for (i, row) in self.0.iter().enumerate() {
+//             for (j, &cell) in row.iter().enumerate() {
+//                 let width = cell.width().unwrap_or(1);
+//                 let padding = 3 - width;
+//                 write!(f, " {}{} ", cell, " ".repeat(padding))?;
+//                 if j < 2 {
+//                     write!(f, "|")?;
+//                 }
+//             }
+//             if i < 2 {
+//                 writeln!(f, "\n-----+-----+-----")?;
+//             }
+//         }
+//         Ok(())
+//     }
+// }
+
 ///Save some .0 calls to get around the tuple
 impl Index<usize> for Grid {
-    type Output = [char; 3];
+    type Output = Cell;
     fn index(&self, index: usize) -> &Self::Output {
+        if index > 9 {
+            panic!("Attempted to index the Grid immutably with an out-of-bounds index");
+        }
         &self.0[index]
     }
 }
@@ -36,6 +63,9 @@ impl Index<usize> for Grid {
 ///Save some .0 calls to get around the tuple, this time for modifying the grid
 impl IndexMut<usize> for Grid {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if index > 9 {
+            panic!("Attempted to index the Grid mutably with an out-of-bounds index");
+        }
         &mut self.0[index]
     }
 }
@@ -43,13 +73,13 @@ impl IndexMut<usize> for Grid {
 ///Easier creation
 impl Default for Grid {
     fn default() -> Self {
-        Self([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
+        Self([Cell::Uninitialised; 9])
     }
 }
 
 ///Used to handle the possible grid states
 pub(crate) enum States {
-    Win(char),
+    Win(Icon),
     Draw,
     Ongoing
 }
@@ -62,25 +92,20 @@ impl Grid {
     ];
     ///Check for wins, draws, or ongoing
     pub(crate) fn get_state(&self) -> States {
-        //Create a 1 by 9 representation of the grid for easier state checking
-        let mut flattened: [char; 9] = ['a'; 9];
-        let mut i: usize = 0;
-        for &row in self.0.iter() {
-            for &cell in row.iter() {
-                flattened[i] = cell;
-                i += 1;
-            }
-        }
+        let grid = &self.0;
         //If the grid signature matches any winning position, return a Win, along with the winner
         for &pos in Self::WINNING_POSITIONS.iter() {
-            if flattened[pos[0]] == flattened[pos[1]] && 
-                flattened[pos[1]] == flattened[pos[2]] {
-                return States::Win(flattened[pos[0]]);
+            if grid[pos[0]] == grid[pos[1]] && 
+                grid[pos[1]] == grid[pos[2]] {
+                return match grid[pos[0]] {
+                    Cell::Cpu(icon)|Cell::Player(icon) => States::Win(icon),
+                    Cell::Uninitialised => unreachable!()
+                }
             }
         }
         //If there is an unselected cell, the game is still ongoing!
-        for cell in flattened {
-            if cell.is_numeric() {
+        for &cell in grid {
+            if let Cell::Uninitialised = cell {
                 return States::Ongoing;
             }
         }
